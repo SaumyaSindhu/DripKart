43f43cd9f5b7c2cf74f8728fb4d5131a85d011f4
Saumya Sindhu <saumyasindhu75@gmail.com>
2026-04-30 21:50:21 +0530

    redux setup for auth

---
+++ Frontend/src/app/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import './App.scss';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>DripKart</h1>
+    </>
+  )
+}
+
+export default App
