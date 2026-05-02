ccb8d7341da52f85b3c1c297e4d3031c7f9dcd61
Saumya Sindhu <saumyasindhu75@gmail.com>
2026-05-02 16:41:56 +0530

    created authenticateSeller middleware

--- Backend/src/routes/product.routes.js
+++ Backend/src/routes/product.routes.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import { Router } from 'express';
+import { authenticateSeller } from '../middlewares/auth.middleware.js';
 
 const productRouter = Router();
 
-
+productRouter.post("/", authenticateSeller)
 
 export default productRouter;
