42c7c41e6388870490d252343f4c7d7a4be05ba9
Saumya Sindhu <saumyasindhu75@gmail.com>
2026-05-19 23:26:17 +0530

    created service layer for cart

---
+++ Frontend/src/features/cart/service/cart.api.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const cartApiInstance = axios.create({
+    baseURL: "/api/cart",
+    withCredentials: true
+})
+
+export async function addItem({ productId, variantId }) {
+    const response = await cartApiInstance.post(`/add/${productId}/${variantId}`);
+
+    return response.data;
+}
