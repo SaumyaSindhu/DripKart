e218b91ae7fec26fa8c47da2c54d60304fa0f765
Saumya Sindhu <saumyasindhu75@gmail.com>
2026-05-19 22:03:12 +0530

    added addToCart validator

---
+++ Backend/src/routes/cart.routes.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import { authenticateUser } from '../middlewares/auth.middleware.js';
+import { validateAddToCart } from '../validators/cart.validator.js';
+
+const cartRouter = Router();
+
+/**
+ * @route POST /api/cart/add/:productId/:variantId
+ * @desc Add item to cart
+ * @access Private
+ * @argument productId - ID of the product to add
+ * @argument variantId - ID of the variant to add
+ * @argument quantity - Quantity of the item to add (optional, default: 1)
+ */
+cartRouter.post("/add/:productId/:variantId", authenticateUser, validateAddToCart)
+
+export default cartRouter;
