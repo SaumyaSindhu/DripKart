4ca4e1be30bcbe9083db13004a1f7264b546fada
Saumya Sindhu <saumyasindhu75@gmail.com>
2026-05-22 11:26:46 +0530

    add incrementCartItem in slice for state management

--- Frontend/src/features/cart/state/cart.slice.js
+++ Frontend/src/features/cart/state/cart.slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit" ;
+import { incrementCartItem } from "../service/cart.api";
 
 const cartSlice = createSlice({
     name: "cart",
@@ -11,9 +12,20 @@ const cartSlice = createSlice({
         },
         addItem: (state, action) => {
             state.items.push(action.payload)
+        },
+        incrementCartItem: (state, action) => {
+            const { productId, variantId } = action.payload
+
+            state.items = state.items.map(item => {
+                if (item.productId === productId && item.variantId === variantId) {
+                    return { ...item, quantity: item.quantity + 1 }
+                } else {
+                    return item
+                }
+            })
         }
     }
 })
 
-export const{ setCart, addItem } = cartSlice.actions;
+export const{ setCart, addItem, incrementCartItem } = cartSlice.actions;
 export default cartSlice.reducer;
